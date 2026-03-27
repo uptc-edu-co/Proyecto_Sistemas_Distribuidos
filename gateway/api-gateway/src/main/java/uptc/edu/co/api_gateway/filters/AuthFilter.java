6afe764bf2b23081d130bb33641b4daeb02b1d10
KimberlyFigueroa21 <kimfigueroa210004@gmail.com>
2026-03-26 22:31:10 -0500

    feat(gateway): implementacion de una puerta de enlace API centralizada con enrutamiento y validación de token Bearer base.

---
+++ gateway/api-gateway/src/main/java/uptc/edu/co/api_gateway/filters/AuthFilter.java
@@ -0,0 +1,33 @@
+package uptc.edu.co.api_gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.HttpStatus;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String authHeader = exchange.getRequest()
+                                    .getHeaders()
+                                    .getFirst("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1; // se ejecuta primero
+    }
+}
